348bdc87c3142313fb98be93708e8219dead6263
Arpan NLN <arpannln@ucla.edu>
2018-05-10 15:13:05 -0700

    initial approach

---
+++ solver.js
@@ -0,0 +1,11 @@
+//initialize global array to memoize our solutions
+var Fibholder = [0, 1];
+
+//building bottom up so we minimize # of operations
+var findNthFib = (n) => {
+  //return if we have already solved it
+  if (Fibholder[n]) return Fibholder[n];
+
+  //build solutions for what we don't have yet
+  
+};
