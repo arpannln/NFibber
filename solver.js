850cb0acceba40f8455e5021e38891e9b515821e
Arpan NLN <arpannln@ucla.edu>
2018-05-10 16:16:54 -0700

    at least my code is functional even if i am not

--- solver.js
+++ solver.js
@@ -2,10 +2,23 @@
 var Fibholder = [0, 1];
 
 //building bottom up so we minimize # of operations
-var findNthFib = (n) => {
-  //return if we have already solved it
-  if (Fibholder[n]) return Fibholder[n];
+const returnNthFib = (n) => {
+  //build our solution space up to n if needed
+  buildUpToN(n);
 
-  //build solutions for what we don't have yet
-  
+  return Fibholder[n];
+};
+
+
+//building bottom up so we minimize # of operations
+const buildUpToN = (n) => {
+  if ( n === Fibholder.length - 1 ) return;
+
+  let lastEl = Fibholder[Fibholder.length - 1];
+  let secondLastEl = Fibholder[Fibholder.length - 2];
+  let nextEl = lastEl + secondLastEl;
+
+  Fibholder.push(nextEl);
+
+  buildUpToN(n);
 };
