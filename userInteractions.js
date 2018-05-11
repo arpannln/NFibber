7acdaedfb0f5f86bc66ca58b044ff66dc742bed7
Arpan NLN <arpannln@ucla.edu>
2018-05-11 09:55:05 -0700

    Nvidia colors feels right here

--- userInteractions.js
+++ userInteractions.js
@@ -55,9 +55,41 @@ const flashError = () => {
   answerContainer.classList.add('error');
 };
 
+//this gets a bit tricky
+
 const updateCanvas = (userInput) => {
   let canvas = document.getElementById('myCanvas');
   let ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, 500, canvas.height);
+  let i = 1;
+  let scale = 100 / userInput;
+  ctx.beginPath();
+
+  while (i <= userInput) {
+    let offset = scale * (Fibholder[i] - Fibholder[i - 1]);
+    switch ( i % 4 ) {
+      case 0:
+        ctx.arc(250 + offset, 250, scale * Fibholder[i], 1.5*Math.PI, 2*Math.PI);
+        ctx.stroke();
+        break;
+      case 1:
+        console.log(offset);
+        ctx.arc(250, 250, scale * Fibholder[i], 0, 0.5*Math.PI);
+        ctx.stroke();
+        break;
+      case 2:
+        ctx.arc(250, 250, scale * Fibholder[i], .5*Math.PI, Math.PI);
+        ctx.stroke();
+        break;
+      case 3:
+        ctx.arc(250 + offset, 250 - offset, scale * Fibholder[i], Math.PI, 1.5*Math.PI);
+        ctx.stroke();
+        break;
+    }
+
+    i += 1;
+
+  }
   //arc(x,y,r,startangle,endangle)
  //one round of spirals is this
  //ctx.beginPath();
