e6864976bb73c796823d0635f2bae65a1aae7872
Arpan NLN <arpannln@ucla.edu>
2018-05-12 15:41:43 -0700

    fixed crashing issue

--- src/userInteractions.js
+++ src/userInteractions.js
@@ -70,25 +70,65 @@ const randomGreen = () => {
 };
 
 const updateCanvas = (userInput) => {
-  let canvas = document.getElementById('myCanvas');
-  let ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+   let canvas = document.getElementById('myCanvas');
+   let ctx = canvas.getContext("2d");
+   ctx.clearRect(0, 0, 500, canvas.height);
+   let scale = 10 / userInput;
+   ctx.lineWidth = 5;
+   let i = 2;
 
-  //we want our presentation to scale with the magnitude  of user input
-  let scale = 10 / userInput;
-  ctx.lineWidth = 4;
-  let i = 1;
-  let angles =[1.5*Math.PI, Math.PI, 0.5*Math.PI, 0];
+   ctx.beginPath();
+   ctx.strokeStyle = randomGreen();
+   ctx.arc(centerX, centerY, scale * Fibholder[1], 2*Math.PI, 1.5*Math.PI, true);
+   ctx.stroke();
 
-  //use canvas to draw spirals representing Fibonacci numbers.
-  //rotating through Fibnumbers as well as associated angles
-  while (i <= userInput) {
-    ctx.beginPath();
-    ctx.strokeStyle = randomGreen();
-    let offset = scale * (Fibholder[i] - Fibholder[i - 1]);
-    ctx.arc(centerX, centerY, scale * Fibholder[i], angles[i % 4], angles[ (i+1) % 4], true);
-    ctx.stroke();
+   while (i <= userInput) {
+     let offset = scale * (Fibholder[i] - Fibholder[i - 1]);
+     ctx.beginPath();
+     ctx.strokeStyle = randomGreen();
+     switch ( i % 4 ) {
+       case 0:
+         ctx.arc(centerX, centerY - offset, scale * Fibholder[i], 0.5*Math.PI, 0, true);
+         ctx.stroke();
+         break;
+       case 1:
+         ctx.arc(centerX - offset, centerY, scale * Fibholder[i], 2*Math.PI, 1.5*Math.PI, true);
+         ctx.stroke();
+         break;
+       case 2:
+         ctx.arc(centerX, centerY + offset, scale * Fibholder[i], 1.5*Math.PI, Math.PI, true);
+         ctx.stroke();
+         break;
+       case 3:
+         ctx.arc(centerX + offset, centerY, scale * Fibholder[i], Math.PI, 0.5*Math.PI, true);
+         ctx.stroke();
+         break;
+     }
 
-    i += 1;
-  }
+     i += 1;
+
+   }
+
+// const updateCanvas = (userInput) => {
+//   let canvas = document.getElementById('myCanvas');
+//   let ctx = canvas.getContext('2d');
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//
+//   //we want our presentation to scale with the magnitude  of user input
+//   let scale = 10 / userInput;
+//   ctx.lineWidth = 4;
+//   let i = 1;
+//   let angles =[1.5*Math.PI, Math.PI, 0.5*Math.PI, 0];
+//
+//   //use canvas to draw spirals representing Fibonacci numbers.
+//   //rotating through Fibnumbers as well as associated angles
+//   while (i <= userInput) {
+//     ctx.beginPath();
+//     ctx.strokeStyle = randomGreen();
+//     let offset = scale * (Fibholder[i] - Fibholder[i - 1]);
+//     ctx.arc(centerX, centerY, scale * Fibholder[i], angles[i % 4], angles[ (i+1) % 4], true);
+//     ctx.stroke();
+//
+//     i += 1;
+//   }
 };
