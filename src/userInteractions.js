682f639227f24e3c380a44054f7c4e6fdedf3866
Arpan NLN <arpannln@ucla.edu>
2018-05-11 19:51:24 -0700

    need some test cases

--- src/userInteractions.js
+++ src/userInteractions.js
@@ -1,9 +1,11 @@
 //initialize global array to memoize our solutions
 var Fibholder = [0, 1];
 
-//building bottom up so we minimize # of operations
 const returnNthFib = (n) => {
-  //build our solution space up to n if needed
+  if ( n < 0 ) {
+    return null;
+  }
+  
   buildUpToN(n);
 
   return Fibholder[n];
@@ -23,7 +25,7 @@ const buildUpToN = (n) => {
   buildUpToN(n);
 };
 
-
+//update page based on user interaction and error handle
 const updateVisuals = () => {
   let userInput = document.getElementById('userInput').value;
   if (isValidInput(userInput)) {
@@ -35,93 +37,80 @@ const updateVisuals = () => {
   updateCanvas(userInput);
 };
 
+const centerX = 145;
+const centerY = 80;
+
 const updateAnswer = (userInput) => {
   let answerContainer = document.getElementById('answer');
   answerContainer.classList.remove('error');
   let answer = returnNthFib(userInput);
-
-  answerContainer.innerHTML = `The ${userInput}th Fibonacci number is... ${answer}`;
-
+  answerContainer.innerHTML = `${answer}`;
   return answer;
 };
 
 const isValidInput = (userInput) => {
-  return userInput >= 0;
+  return userInput !== '' && userInput >= 0;
 };
 
 const flashError = () => {
   let answerContainer = document.getElementById('answer');
-  answerContainer.innerHTML = "See why n can't be negative below";
+  answerContainer.innerHTML = 'Please enter a positive number :)';
   answerContainer.classList.add('error');
 };
 
-
-//this gets a bit tricky
+//decent shades of green
 const greens = [
-  "#76b900",
-  "#85c01c",
-  "#94c838",
-  "#a3d055",
-  "#b2d871",
-  "#68a400",
-  "#5b8f00",
-  "#4e7b00",
-  "#416600",
-  "#345200"
+  '#76b900',
+  '#85c01c',
+  '#94c838',
+  '#a3d055',
+  '#b2d871',
+  '#68a400',
+  '#5b8f00',
+  '#4e7b00',
+  '#416600',
+  '#345200'
 ];
-
-const center = {
-  x: 145,
-  y: 70
-};
 
 const randomGreen = () => {
   return greens[Math.floor(Math.random() * greens.length)];
-
 };
 
 const updateCanvas = (userInput) => {
   let canvas = document.getElementById('myCanvas');
-  let ctx = canvas.getContext("2d");
+  let ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, 500, canvas.height);
-  let scale = 50 / userInput;
-  ctx.lineWidth = 5;
-  let i = 3;
 
-  ctx.beginPath();
-  ctx.strokeStyle = randomGreen();
-  ctx.arc(center['x'], center['y'], scale * Fibholder[1], Math.PI, 1.5*Math.PI);
-  ctx.stroke();
+  //we want our presentation to scale with the magnitude  of user input
+  let scale = 10 / userInput;
+  ctx.lineWidth = 4;
+  let i = 1;
 
-  ctx.beginPath();
-  ctx.strokeStyle = randomGreen();
-  ctx.arc(center['x'], center['y'], scale * Fibholder[2], 1.5*Math.PI, 2*Math.PI);
-  ctx.stroke();
-
+  //use canvas to draw spirals representing Fibonacci numbers.
   while (i <= userInput) {
     ctx.beginPath();
     ctx.strokeStyle = randomGreen();
     let offset = scale * (Fibholder[i] - Fibholder[i - 1]);
+
     switch ( i % 4 ) {
       case 0:
-        ctx.arc(center['x'] - offset, center['y'] - offset, scale * Fibholder[i], 0.5*Math.PI, Math.PI);
+        ctx.arc(centerX, centerY, scale * Fibholder[i], Math.PI, 0.5*Math.PI, true);
         ctx.stroke();
         break;
       case 1:
-        ctx.arc(center['x'], center['y'], scale * Fibholder[i], Math.PI, 1.5*Math.PI);
+        ctx.arc(centerX, centerY, scale * Fibholder[i], 0.5*Math.PI, 0*Math.PI, true);
         ctx.stroke();
         break;
       case 2:
-        ctx.arc(center['x'], center['y'], scale * Fibholder[i], 1.5*Math.PI, 2*Math.PI);
+        ctx.arc(centerX, centerY, scale * Fibholder[i], 2*Math.PI, 1.5*Math.PI, true);
         ctx.stroke();
         break;
       case 3:
-        ctx.arc(center['x'] - offset, center['y'], scale * Fibholder[i], 0, 0.5*Math.PI);
+        ctx.arc(centerX, centerY, scale * Fibholder[i], 1.5*Math.PI, Math.PI, true);
         ctx.stroke();
         break;
     }
 
     i += 1;
-
   }
 };
